c228aadb80271a1d42320504fbc30738eb059766
Niels Drost <nielsdrost7@gmail.com>
2017-05-28 10:53:38 +0200

    Initial commit (StartLaravel with Laravel 5.3)

---
+++ routes/web.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('home');
+});
+
+Route::get('/charts', function()
+{
+	return View::make('mcharts');
+});
+
+Route::get('/tables', function()
+{
+	return View::make('table');
+});
+
+Route::get('/forms', function()
+{
+	return View::make('form');
+});
+
+Route::get('/grid', function()
+{
+	return View::make('grid');
+});
+
+Route::get('/buttons', function()
+{
+	return View::make('buttons');
+});
+
+
+Route::get('/icons', function()
+{
+	return View::make('icons');
+});
+
+Route::get('/panels', function()
+{
+	return View::make('panel');
+});
+
+Route::get('/typography', function()
+{
+	return View::make('typography');
+});
+
+Route::get('/notifications', function()
+{
+	return View::make('notifications');
+});
+
+Route::get('/blank', function()
+{
+	return View::make('blank');
+});
+
+Route::get('/login', function()
+{
+	return View::make('login');
+});
+
+Route::get('/documentation', function()
+{
+	return View::make('documentation');
+});
